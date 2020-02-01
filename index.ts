9696396163bd67fbc639645232b4cd7c6cf0f756
Cem Derin <cem@wysiwyg.de>
2020-02-01 21:20:55 +0100

    fix: 🐛 Add weapon to global export

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ import Phase from "./src/Phase";
 import Logger from './src/Logger';
 import Model from "./src/Model";
 import Throw from "./src/Throw";
+import Weapon from './src/Weapon';
 
 export {
     Phase as default,
     Phase,
     Logger,
     Model,
-    Throw
+    Throw,
+    Weapon
 };
